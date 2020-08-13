867e78e039a9b9db2ae6a0314becb8ca17aeb117
rishithatumu <rishithatumu@gmail.com>
2020-08-13 12:11:26 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/webapplication1/Webapplication1Application.java
@@ -0,0 +1,14 @@
+package com.springboot.webapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapplication1Application {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(Webapplication1Application.class, args);
+	}
+
+}
